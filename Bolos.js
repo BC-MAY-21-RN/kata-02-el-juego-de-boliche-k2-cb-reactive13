9bf46ccf65d241fd538ac2f7f9afcd85401737df
cleon5 <cleon5@ucol.mx>
2022-08-09 17:35:52 -0500

    Bolos

--- Bolos.js
+++ Bolos.js
@@ -12,7 +12,8 @@ class Bolos{
         [7,3],
         [6,4],
         [10,0],
-        [2,6,2]]
+        [2,8,6],
+        [0,0,0]]
         this.cont = 0;
         this.points=[];
         this.score = []
@@ -22,25 +23,29 @@ class Bolos{
         for(let i = 0; i < this.pinsDown.length; i++) {
             if(i == 9) this.points.push(this.pinsDown[i][0]+this.pinsDown[i][1] +this.pinsDown[i][2]);
             else this.points.push(this.pinsDown[i][0]+this.pinsDown[i][1]);
-            
         }
         return this.points
     }
 
     calcScore() {
-        let x;
-        for(let i = 0; i < this.points.length; i++) {
-            if(i==0) this.score.push(this.points[i])
-            else this.score.push(this.points[i] + this.score[i-1])
-            
+        for(let i = 0; i < this.points.length -1; i++) {
+            if(this.pinsDown[i][0]==10) this.evStrike(i)
+            else if(this.points[i]==10) this.evSpare(i)
+            else this.evAdd(i)
         }
         return this.score
     }
-    evStrike(x){
-        
+    evStrike(i){
+        if(i==0) this.score.push(this.points[i] + this.pinsDown[i+1][0] +this.pinsDown[i+1][1])
+         else this.score.push(this.points[i] + this.score[i-1] + this.pinsDown[i+1][0] +this.pinsDown[i+1][1])
     }
-    evSpare(){
-
+    evSpare(i){
+        if(i==0) this.score.push(this.points[i] + this.pinsDown[i+1][0])
+        else this.score.push(this.points[i] + this.pinsDown[i+1][0] + this.score[i-1])
+    }
+    evAdd(i){
+        if(i==0) this.score.push(this.points[i])
+        else this.score.push(this.points[i] + this.score[i-1])
     }
 }
 
