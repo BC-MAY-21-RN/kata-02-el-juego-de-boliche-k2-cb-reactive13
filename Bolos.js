3268a526ea7c8f087ca639f7be6e62cc694b135f
cleon5 <cleon5@ucol.mx>
2022-08-11 16:19:39 -0500

    feat(Bolos): Generate random score
    Co-authored-by: Charly77955 <desantiscarlos95@gmail.com>
    Co-authored-by: wiltrouble <wiltrouble@gmail.com>
    Co-authored-by: Astroyayo <lugo044@gmail.com>

--- Bolos.js
+++ Bolos.js
@@ -10,6 +10,30 @@ class Bolos {
         this.points = []
         this.score = []
     }
+    calcMatriz() {
+        let control = [];
+        let pins = []; 
+        let x = 0;
+        for (let i = 0; i < 10; i++) {
+          control = [];
+          if (i == 9) {
+            control.push(Math.floor(Math.random() * 11));
+            control.push(Math.floor(Math.random() * (11 - control[0])));
+            x = (control[0] + control[1])
+            if (x == 10)
+                control.push(Math.floor(Math.random() * 11));
+            else 
+                control.push(0)
+            pins.push(control);
+          } else {
+            control.push(Math.floor(Math.random() * 11));
+            control.push(Math.floor(Math.random() * (11 - control[0])));
+            pins.push(control);
+          }
+        }
+        this.pinsDown = pins;
+        return this.pinsDown;
+      }
 
     calcPoints() {
         for (let i = 0; i < this.pinsDown.length; i++) {
@@ -60,5 +84,9 @@ class Bolos {
         else this.score.push(this.points[i] + this.score[i - 1])
     }
 }
+let bolos = new Bolos();
+console.table(bolos.calcMatriz());
+console.log(bolos.calcPoints());
+console.log(bolos.calcScore());
 
 module.exports = { Bolos }
